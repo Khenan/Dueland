0aca86eedacc338859588898cbb481ff6ce72e68
Khenan Perso <khenan.benanou@gmail.com>
2025-03-23 16:04:49 +0100

    [FEAT] Rpc ping

--- Assets/Multiplayer/GameManager.cs
+++ Assets/Multiplayer/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -5,6 +6,8 @@ public class GameManager : NetworkBehaviour
 {
     private NetworkVariable<float> gameTime = new(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     public float GameTime => gameTime.Value;
+
+    public static Action<GameManager> onGameManagerSpawned;
 
     private static GameManager instance;
     public static GameManager Instance
@@ -21,15 +24,33 @@ public class GameManager : NetworkBehaviour
 
     void Update()
     {
-        if(NetworkManager.Singleton.IsServer)
+        if (NetworkManager.Singleton.IsServer)
         {
             gameTime.Value += Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            PingServer();
         }
     }
 
     public override void OnNetworkSpawn()
     {
-        Debug.Log("Network Object Spawned");
-        MultiplayerManager.Instance.onGameManagerSpawned?.Invoke(this);
+        Debug.Log("Network GameManager Spawned");
+        onGameManagerSpawned?.Invoke(this);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void PingServer()
+    {
+        Debug.Log("A client ping Server");
+        PingClient();
+    }
+
+    [ClientRpc]
+    private void PingClient()
+    {
+        Debug.Log("Server ping Client");
     }
 }
