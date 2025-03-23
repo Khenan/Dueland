db01bc4e84252e06b6b65526fb02f9f84241c834
RemyAlmar <r.almar1997@gmail.com>
2025-03-23 16:23:29 +0100

    [FIX] Server RPC

--- Assets/UI/AddInt.cs
+++ Assets/UI/AddInt.cs
@@ -6,31 +6,25 @@ using Unity.VisualScripting;
 public class AddInt : NetworkBehaviour, IDependantToNetworkCanvas
 {
     private TMP_Text m_text;
-    private NetworkVariable<int> value = new(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    private NetworkVariable<int> value = new(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     public int Value => value.Value;
 
     void Awake()
     {
         m_text = GetComponentInChildren<TMP_Text>();
+        value.OnValueChanged += UpdateText;
     }
     public void AddValue()
     {
         value.Value++;
-        UpdateText();
     }
-    private void UpdateText()
+    private void UpdateText(int currentValue, int newValue)
     {
-        m_text.text = value.Value.ToString();
+        m_text.text = newValue.ToString();
     }
 
     [ServerRpc(RequireOwnership = false)]
     public void PingServerRpc()
-    {
-        PingClientRpc();
-    }
-
-    [ClientRpc]
-    private void PingClientRpc()
     {
         AddValue();
     }
